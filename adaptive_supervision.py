d4744b9b14fca21b75de5e2cf9da1bfc44583c27
Olof Bourghardt <ob655088@gmail.com>
2022-03-17 10:56:01 +0100

    Add help function to find closest box to an annotation as well as write code for pseudo labels. Add a test for find_closest_box

--- adaptive_supervision.py
+++ adaptive_supervision.py
@@ -1,4 +1,5 @@
 
+from gettext import find
 import math
 
 def active_smapling():
@@ -13,22 +14,37 @@ def dist_from_point(box, weak_annotation):
     box_x2 = box[2]
     box_y1 = box[1]
     box_y2 = box[3]
-    box_center = (abs(box_x2 - box_x1)/2.0, abs(box_y1 - box_y2)/2.0)
+    box_width = box_x2 - box_x1
+    box_height = box_y2 - box_y1
+    box_center = (box_x1 + box_width/2.0, box_y1 + box_height/2.0)
     dist = math.sqrt(math.pow(box_center[0] - weak_annotation[0], 2) + math.pow(box_center[1] - weak_annotation[1], 2))
     return dist
 
+#finds the bounding box with its center closest to the weak annotation
+def find_closest_box(bounding_boxes, annotation):
+    closest_box = []
+    closest_distance = float('inf')
+    for box in bounding_boxes:
+        dist = dist_from_point(box, annotation)
+        if dist < closest_distance:
+            closest_box = box
+            closest_distance = dist
+    return closest_box
+
 def pseudo_labels(model, sample, weak_annotations):
-    for image, weak_annotation in zip(sample, weak_annotations):
-        boxes, scores, classes, nums = model.predict(image)
-
-
     #predict bounding boxes
     #use weak labels to choose best possible bounding box
     #   - for every click location, we pseudo label that object with a
     #     bounding-box with center closest to the click location.
     #   - The object is classified as the class with the
     #     highest probability for the chosen bounding box
-    return
+    chosen_bounding_boxes = []
+    for image, annotations in zip(sample, weak_annotations):
+        boxes, _, _, _ = model.predict(image)
+        for annotation in annotations:
+            closest_box = find_closest_box(boxes, annotation)
+            chosen_bounding_boxes.append(closest_box)
+    return chosen_bounding_boxes
 
 def query_annotations():
     return
--- adaptive_supervision_test.py
+++ adaptive_supervision_test.py
@@ -1,12 +1,19 @@
+from __future__ import annotations
 import unittest
 import math
-from adaptive_supervision import dist_from_point
+from adaptive_supervision import dist_from_point, find_closest_box
 
 class TestDistFromPoint(unittest.TestCase):
     def test_dist_from_point(self):
         box = [0.0, 0.0, 4.0, 4.0]
         annotation = (3.0, 1.0)
         self.assertEqual(dist_from_point(box, annotation), math.sqrt(2), "Should be square root of 2")
+
+class TestFindClosestBox(unittest.TestCase):
+    def test_find_closes_box(self):
+        boxes = [[0.0, 0.0, 4.0, 4.0], [6.0, 5.0, 8.0, 7.0], [3.0, 1.0, 7.0, 5.0]]
+        annotation = (4.0, 4.0)
+        self.assertEqual(find_closest_box(boxes, annotation), boxes[2], "Should be [3.0, 1.0, 7.0, 5.0]")
 
 if __name__ == "__main__":
     unittest.main()
