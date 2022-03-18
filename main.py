6625736d770c5fc2a3d0a3cbb3710ca403fa6f9c
albernnV <36505023+albernnV@users.noreply.github.com>
2022-03-18 15:01:14 +0100

    made mvc design pattern along with query_weak_annotations and query_annotations

---
+++ main.py
@@ -0,0 +1,24 @@
+
+import tkinter as tk
+import os
+from model import Model
+from view import View
+from controller import Controller
+
+def main():
+    set_images = []
+    path = "D:/Voi/cv-pipeline/cv-pipline/images"
+    for image in os.listdir(path):
+        set_images.append(image)
+    
+    window = tk.Tk()
+    
+    view = View(window)
+    model = Model(view,set_images,path)
+    controller = Controller(model,view)
+    controller.start_ui()
+    window.mainloop()
+    circle_coords = model.get_circle_coords()
+    print(circle_coords)
+if __name__ == "__main__":
+    main()
