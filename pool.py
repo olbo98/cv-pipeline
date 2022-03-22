80b8e9178c66d9dd6b8af1b44784bffdcd1e3a61
Olof Bourghardt <ob655088@gmail.com>
2022-03-22 15:42:04 +0100

    Create Pool class

---
+++ pool.py
@@ -0,0 +1,28 @@
+from random import sample
+
+
+class Pool():
+    def __init__(self):
+        self._images = []
+        self._labels = []
+
+    @property
+    def len(self):
+        return len(self._images)
+
+    def get_sample(self, index):
+        sample = ''
+        label = []
+        if self.len > 0:
+            sample = self._images[index]
+            label = self._labels[index]
+        return sample, label
+
+    def add_sample(self, image, label):
+        self._images.append(image)
+        self._labels.append(label)
+
+    def delete_sample(self, image):
+        index = self._images.index(image)
+        self._images.remove(image)
+        del self._labels[index]
