0ecc0aa098f3f261c059efbfd52e627c8027900e
Olof Bourghardt <ob655088@gmail.com>
2022-04-29 11:02:58 +0200

    Disable tensorflow taking up all available space

--- module.py
+++ module.py
@@ -26,6 +26,10 @@ import shutil
 import random
 from mapcalc import calculate_map
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
 class Module():
     """
     A class that represents the adaptive super vision module. 
